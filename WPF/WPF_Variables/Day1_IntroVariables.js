8c31793eafad4d0b7a1dc371dc9a3e96f19176b0
KaseKreative <KaseKreative@fullsail.edu>
2014-01-08 21:40:31 -0500

    Problem Solving And Variables
    In Class work and Problem Solving Assignment

---
+++ WPF/WPF_Variables/Day1_IntroVariables.js
@@ -0,0 +1,119 @@
+/**
+ * Created by KaseTebo on 1/8/14.
+ */
+    // highlight cmd key +  /
+    // to make comments
+
+//var numberOfTreesToCutDown=4;
+//var x="Is a great letter!";
+//var escapeCharacters="\"blahblahblah\"";
+//
+//console.log(x);
+//
+//var Programmers=6;
+//var Players=10;
+//var Challengers=5;
+//var Cookies="Making cookies for dessert.";
+//var Cooks="To many cooks in the kitchen,";
+//var Barbecue=" and there is a fourth of july barbecue.";
+//
+//console.log(Cookies);
+//
+//console.log(escapeCharacters);
+//    //Addition
+//console.log(Players+Challengers);
+//    // Connecting Strings
+//console.log(Cooks+Barbecue);
+//
+//var e=3;
+//var o=9;
+//
+//    // Multiplication
+//
+//console.log(Challengers*e);
+//
+//    // Divison
+//
+//var Division=(9/3);
+//
+//console.log(Division);
+//console.log(o/e);
+//
+//    // Modulas
+//
+//var Modulas=9%3;
+//console.log(o%e);
+//console.log(Modulas);
+//
+//    // Prompt Asking Consumer For Information
+//        // ParseInt(); <--- Turns String Into An Integer
+//var wasANumber=prompt("Place a Number here");
+//console.log(3+parseInt(wasANumber));
+//
+//    // Adding Two Prompted Variables Together
+//var Number1=parseInt(prompt("What is your age?"));
+//var Number2=parseInt(prompt("How old will you be next year?"));
+//
+//console.log(Number1+Number2);
+//
+//    // Turning a number into a string.
+//var Empty="";
+//console.log(e+Empty);
+//console.log(3+"");
+//
+//    // Half Numbers .5
+//        //parseFloat(); Is for decimals
+//var halfNumber=prompt("What is your favorite partial number?");
+//console.log(parseFloat(halfNumber) +10);
+//
+//    // Create variables
+//    // Manipulate information in variables
+//    // Variables interact
+//    // Log all information
+//
+//    // Create 3 Variables
+//       // Float
+//        // Integer
+//        // String
+//    // Float + Integer = Interact then store into new variable
+//    // Interact the new variable with the string
+//    // String must describe what happened. "I added two numbers and ended up with ________ ."
+//
+//    // I performed multiplication on ______ and _______, and the answer was ________ .
+//
+//var Float=parseFloat(prompt("What is your favorite partial number?"));
+//var Int=parseInt(prompt("How old are you?"));
+//var String="I multiplied two variables and got ";
+//var FloatInt= Float*Int ;
+//console.log(String+FloatInt);
+//
+//var IPerformedMult= "I performed multiplication on ";
+//var and1= " and ";
+//var AndTheAnswer= " and the answer was ";
+//
+//console.log(IPerformedMult+Float+and1+Int+AndTheAnswer+FloatInt);
+
+// Array
+
+var shoppingList=["Eggs","Chicken","BrownRice","Veg"];
+console.log(shoppingList);
+//console.log(shoppingList[0]);
+//console.log(shoppingList[1]);
+//console.log(shoppingList[2]);
+//console.log(shoppingList[3]);
+//console.log(shoppingList[4]);
+
+
+shoppingList.push("Bacon");
+//shoppingList.pop();
+console.log(shoppingList);
+console.log(shoppingList.length);
+//console.log(shoppingList[0]);
+//console.log(shoppingList[1]);
+//console.log(shoppingList[2]);
+//console.log(shoppingList[3]);
+//console.log(shoppingList[4]);
+//console.log(shoppingList[5]);
+
+
+
