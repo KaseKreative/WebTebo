bb21bebbd780a8a7906bcfff486d998e7eeac97b
KaseKreative <KaseKreative@fullsail.edu>
2014-01-10 21:40:43 -0500

    DogYears_SliceOfPiePart1

---
+++ WPF/Expressions_Worksheet/JavaScript/Expressions_Worksheet.js
@@ -0,0 +1,35 @@
+/**
+ * Created by KaseTebo on 1/10/14.
+ */
+//          //Assigning the dogs human age to a variable
+//var HumanAge=parseInt(prompt("How old is sparky?(In Human Years)"));
+//          //Multiplying the dogs age by 7 to get the age in dog years.
+//    var DogAge=(HumanAge*7)
+//
+//          //Strings
+//var SparkyIs="Sparky is ";
+//var HumanYearsOldWhichIs=" human years old which is ";
+//var InDogYears=" in dog years."
+//
+            // Everything put together
+//console.log(SparkyIs+HumanAge+HumanYearsOldWhichIs+DogAge+InDogYears);
+
+
+//        //Taking in information through prompts turning them to numbers that are ready to be manipulated
+//var SlicesPerPizza =parseFloat(prompt("How many slices are in the pizza?"));
+//var PizzasOrdered =parseInt(prompt("Number of pizzas ordered?"));
+//var PeopleAtParty =parseInt(prompt("How many people are attending the party?"));
+//
+//
+//              //Multiplying the information that was received
+//var TotalSlices = PizzasOrdered*SlicesPerPizza;
+//              //Dividing the information that was received
+//var SlicesPerPerson =TotalSlices/PeopleAtParty;
+//             //Strings
+//var EachPersonAte="Each person ate ";
+//var SlicesOfPizzaAtTheParty=" slices of pizza at the party.";
+//             //Everything tied together
+//console.log(EachPersonAte+SlicesPerPerson+SlicesOfPizzaAtTheParty)
+
+
+
