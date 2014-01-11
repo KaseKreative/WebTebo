402b86eb493ee350d4b6e98d29a7f28f444369ab
KaseKreative <KaseKreative@fullsail.edu>
2014-01-10 22:53:47 -0500

    Pie2_AverageShopping_Discounts

--- WPF/Expressions_Worksheet/JavaScript/Expressions_Worksheet.js
+++ WPF/Expressions_Worksheet/JavaScript/Expressions_Worksheet.js
@@ -15,21 +15,78 @@
 //console.log(SparkyIs+HumanAge+HumanYearsOldWhichIs+DogAge+InDogYears);
 
 
-//        //Taking in information through prompts turning them to numbers that are ready to be manipulated
+        //Taking in information through prompts turning them to numbers that are ready to be manipulated
 //var SlicesPerPizza =parseFloat(prompt("How many slices are in the pizza?"));
+//var SlicesPerPizza =parseInt(prompt("How many slices are in the pizza?"));
 //var PizzasOrdered =parseInt(prompt("Number of pizzas ordered?"));
 //var PeopleAtParty =parseInt(prompt("How many people are attending the party?"));
-//
-//
-//              //Multiplying the information that was received
+
+
+              //Multiplying the information that was received
 //var TotalSlices = PizzasOrdered*SlicesPerPizza;
-//              //Dividing the information that was received
+              //Dividing the information that was received
 //var SlicesPerPerson =TotalSlices/PeopleAtParty;
-//             //Strings
+             //Strings
 //var EachPersonAte="Each person ate ";
 //var SlicesOfPizzaAtTheParty=" slices of pizza at the party.";
-//             //Everything tied together
+             //Everything tied together
 //console.log(EachPersonAte+SlicesPerPerson+SlicesOfPizzaAtTheParty)
+            //Strings
+//var SparkyGot="Sparky got ";
+//var SlicesOfPizza=" slices of pizza.";
+            //Modulus to figure out the remainder for sparky after people eat.
+//var Modulus=TotalSlices%PeopleAtParty;
+//console.log(SparkyGot+Modulus+SlicesOfPizza);
+
+//        The Array
+//var GroceryTotals=[];
+//        Making it so someone inputs the information into the list.
+//GroceryTotals[0]=parseInt(prompt("Grocery total for week one?"));
+//GroceryTotals[1]=parseInt(prompt("Grocery total for week two?"));
+//GroceryTotals[2]=parseInt(prompt("Grocery total for week three?"));
+//GroceryTotals[3]=parseInt(prompt("Grocery total for week four?"));
+//GroceryTotals[4]=parseInt(prompt("Grocery total for week five?"));
+//        Adding up the total
+//var Total=GroceryTotals[0]+GroceryTotals[1]+GroceryTotals[2]+GroceryTotals[3]+GroceryTotals[4];
+//        Dividing the total from how many weeks there were to find the mean.
+//var Average=Total/5;
+//        Strings
+//var YouHaveSpentATotalOf="You have spent a total of $";
+//var OnGroceriesOver5WeeksThatIsAnAverageOf=" on groceries over five weeks that is an average of $";
+//var PerWeek=" per week.";
+//       Connecting it all together
+//console.log(YouHaveSpentATotalOf+Total+OnGroceriesOver5WeeksThatIsAnAverageOf+Average+PerWeek);
+
+
+//    // Taking in information and assigning it to variables
+//var OriginialPrice =parseFloat(prompt("What is the original price?"));
+//var DiscountPercentage =parseInt(prompt("What is the discount percentage?"));
+//var Description =prompt("What are you trying to purchase?(Shirt,Pants, etc.)");
+//var SalesTax =parseFloat(prompt("What is the sales tax?"));
+//    //changing the number percents to decimals
+//var DecimalTax=SalesTax/100.00
+//var DecimalDiscount= DiscountPercentage/100.00
+//    //Finding out the price with tax
+//var Tax=OriginialPrice*DecimalTax;
+//var PriceWithTax=OriginialPrice+Tax;
+//
+//    //Finding out the price with the discount without tax
+//var DiscountWithoutTax=OriginialPrice*DecimalDiscount;
+//var PriceWithDiscountWithoutTax=OriginialPrice-DiscountWithoutTax;
+//    //Finding out the price with the discount with tax
+//var DiscountWithTax=PriceWithTax*DecimalDiscount;
+//var PriceWithDiscountWithTax=PriceWithTax-DiscountWithTax;
+//
+//
+//    //Strings
+//var Your="Your ";
+//var WasOriginally= " was originally $";
+//var ButAfterA=" but after a ";
+//var DiscountItIsNow= "% discount, it is now $"
+//var WithoutTaxAnd= " without tax, and $"
+//var WithTax= " with tax."
+//    //Bringing everything together
+//console.log(Your+Description+WasOriginally+OriginialPrice+ButAfterA+DiscountPercentage+DiscountItIsNow+PriceWithDiscountWithoutTax+WithoutTaxAnd+PriceWithDiscountWithTax+WithTax);
 
 
 
