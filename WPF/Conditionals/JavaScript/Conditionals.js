90a7b04b69a2bba5f45197725899331c2a107f56
KaseKreative <KaseKreative@fullsail.edu>
2014-01-10 23:10:47 -0500

    StuffYourFace1

---
+++ WPF/Conditionals/JavaScript/Conditionals.js
@@ -0,0 +1,17 @@
+/**
+ * Created by KaseTebo on 1/10/14.
+ */
+
+//Stuff Your Face I
+
+var CompetitorsWeight=parseInt(prompt("What is your weight?"));
+
+//If the competitor is under 250
+
+if (CompetitorsWeight > 250){
+    console.log("The competitor qualifies for the heavyweight division");
+
+} else {
+    console.log("The competitor needs to gain some weight");
+}
+
