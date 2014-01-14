c5c00b0f99abeb67567a811450606c70d44071bb
KaseKreative <KaseKreative@fullsail.edu>
2014-01-13 22:26:51 -0500

    Wacky_Personal_Conditionals

--- WPF/Conditionals_Assignment/Conditionals_Personal/Javascript/Conditionals.js
+++ WPF/Conditionals_Assignment/Conditionals_Personal/Javascript/Conditionals.js
@@ -3,4 +3,20 @@
  */
 
 
+var videoGames=parseInt(prompt("How many video games do you own?"));
+var consoles=parseInt(prompt("How many consoles do you own?"));
 
+if(videoGames>=50){
+        console.log("Wow you have a lot of video games.");
+} else {
+    console.log("A decent amount but you need more games!");
+}
+
+if(consoles==2){
+    console.log("That's about average for consoles.")
+} else {
+    if(consoles>=2){
+        console.log("How's the rich life treating you?")
+    } else {
+        console.log("Get some more consoles!")
+    }}
--- WPF/Conditionals_Assignment/Conditionals_Wacky/Javascript/Conditionals.js
+++ WPF/Conditionals_Assignment/Conditionals_Wacky/Javascript/Conditionals.js
@@ -4,3 +4,25 @@
 
 
 
+var tacos=prompt("What kind of taco do you want?"+"\n"+" Chicken, Beef, Fish");
+var howManyTacos=parseInt(prompt("How many tacos do you want?"));
+
+var fish =6;
+var chicken =5;
+var beef =4;
+
+var chickenPrice = chicken*howManyTacos;
+var beefPrice = beef*howManyTacos;
+var fishPrice = fish*howManyTacos;
+
+if (tacos == "Chicken".toLowerCase() ){
+    console.log("Your price will be $"+chickenPrice);
+        } else {
+            if ( tacos == "Beef".toLowerCase()){
+                console.log("Your price will be $"+beefPrice);
+            } else {
+                if (tacos == "Fish".toLowerCase()){
+                    console.log("Your price will be $"+fishPrice);
+                } else {
+                    console.log("Incorrect type of taco");
+                }}}
