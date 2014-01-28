5c2607e77b250ef59ac7645d0ae652b67b37955e
KaseKreative <KaseKreative@fullsail.edu>
2014-01-28 15:06:58 -0500

    FunctionsAssignment04

--- WPF/Functions_Assignment/Wacky/Javascript/MonsterArrays.js
+++ WPF/Functions_Assignment/Wacky/Javascript/MonsterArrays.js
@@ -10,28 +10,36 @@ dexterity = 3;
 strength = 4;
 
 
+function str (){
+   Str =  Math.floor(3*(Math.random()*6));
+    return Str;
+}
+
+function dex (){
+    Dex = Math.floor(3*(Math.random()*6));
+    return Dex;
+}
+
+function dmg (str){
+    Dmg = Math.floor((Math.random()*6) + str);
+    return Dmg;
+}
 
 // Cave Array
 bear = 0;
 bat = 1;
 yeti = 2;
 skeleton = 3;
-raccoon = 4;
-mudman = 5;
+mudman = 4;
 
 cave = [];
 cave[bear] = [];
 cave[bat] = [];
 cave[yeti] = [];
 cave[skeleton] = [];
-cave[raccoon] = [];
 cave[mudman] = [];
 
- = [type];
- = [health];
- = [damage];
- = [dexterity];
- = [strength];
+
 
 
 cave[bear] = [type];
@@ -40,11 +48,23 @@ cave[bear] = [damage];
 cave[bear] = [dexterity];
 cave[bear] = [strength];
 
+cave[bear][type] = "Bear";
+cave[bear][health] = 20;
+cave[bear][damage]= dmg(str());
+cave[bear][dexterity]= dex();
+cave[bear][strength] = str();
+
 cave[bat] = [type];
 cave[bat] = [health];
 cave[bat] = [damage];
 cave[bat] = [dexterity];
 cave[bat] = [strength];
+
+cave[bat][type] = "Bat";
+cave[bat][health] = 3;
+cave[bat][damage]= dmg(str());
+cave[bat][dexterity]= dex();
+cave[bat][strength] = str();
 
 cave[yeti] = [type];
 cave[yeti] = [health];
@@ -52,17 +72,24 @@ cave[yeti] = [damage];
 cave[yeti] = [dexterity];
 cave[yeti] = [strength];
 
+cave[yeti][type] = "Yeti";
+cave[yeti][health] = 35;
+cave[yeti][damage]= dmg(str());
+cave[yeti][dexterity]= dex();
+cave[yeti][strength] = str();
+
 cave[skeleton] = [type];
 cave[skeleton] = [health];
 cave[skeleton] = [damage];
 cave[skeleton] = [dexterity];
 cave[skeleton] = [strength];
 
-cave[raccoon] = [type];
-cave[raccoon] = [health];
-cave[raccoon] = [damage];
-cave[raccoon] = [dexterity];
-cave[raccoon] = [strength];
+cave[skeleton][type] = "Skeleton";
+cave[skeleton][health] = 5;
+cave[skeleton][damage]= dmg(str());
+cave[skeleton][dexterity]= dex();
+cave[skeleton][strength] = str();
+
 
 cave[mudman] = [type];
 cave[mudman] = [health];
@@ -70,21 +97,33 @@ cave[mudman] = [damage];
 cave[mudman] = [dexterity];
 cave[mudman] = [strength];
 
+cave[mudman][type] = "Mudman";
+cave[mudman][health] = 20;
+cave[mudman][damage]= dmg(str());
+cave[mudman][dexterity]= dex();
+cave[mudman][strength] = str();
+
+
+
+
+
+
+
+
+
 
 
 // Dungeon Array
 skeleton = 0;
-orc = type;
-blob = health;
-skeletonArcher = damage;
-zombie = dexterity;
-beholder = strength;
+orc = 1;
+blob = 2;
+zombie = 3;
+beholder = 4;
 
 dungeon = [];
 dungeon[skeleton] = [];
 dungeon[orc] = [];
 dungeon[blob] = [];
-dungeon[skeletonArcher] = [];
 dungeon[zombie] = [];
 dungeon[beholder] = [];
 
@@ -97,9 +136,9 @@ dungeon[skeleton] = [strength];
 
 dungeon[skeleton][type] = "Skeleton";
 dungeon[skeleton][health] = 5;
-dungeon[skeleton][damage]= "1";
-dungeon[skeleton][dexterity]= "2";
-dungeon[skeleton][strength] = "3";
+dungeon[skeleton][damage]= dmg(str());
+dungeon[skeleton][dexterity]= dex();
+dungeon[skeleton][strength] = str();
 
 dungeon[orc] = [type];
 dungeon[orc] = [health];
@@ -109,9 +148,9 @@ dungeon[orc] = [strength];
 
 dungeon[orc]  [type] = "Orc";
 dungeon[orc]  [health] = 5;
-dungeon[orc]  [damage] = "1";
-dungeon[orc]  [dexterity] = "2";
-dungeon[orc]  [strength] = "3";
+dungeon[orc]  [damage] = dmg(str());
+dungeon[orc]  [dexterity] = dex();
+dungeon[orc]  [strength] = str();
 
 dungeon[blob] = [type];
 dungeon[blob] = [health];
@@ -121,15 +160,9 @@ dungeon[blob] = [strength];
 
 dungeon[blob]  [type] = "Blob";
 dungeon[blob]  [health] = 20;
-dungeon[blob]  [damage] = "1";
-dungeon[blob]  [dexterity] = "2";
-dungeon[blob]  [strength] = "3";
-
-dungeon[skeletonArcher] = [type];
-dungeon[skeletonArcher] = [health];
-dungeon[skeletonArcher] = [damage];
-dungeon[skeletonArcher] = [dexterity];
-dungeon[skeletonArcher] = [strength];
+dungeon[blob]  [damage] = dmg(str());
+dungeon[blob]  [dexterity] = dex();
+dungeon[blob]  [strength] = str();
 
 dungeon[zombie] = [type];
 dungeon[zombie] = [health];
@@ -137,65 +170,104 @@ dungeon[zombie] = [damage];
 dungeon[zombie] = [dexterity];
 dungeon[zombie] = [strength];
 
+dungeon[zombie]  [type] = "Zombie";
+dungeon[zombie]  [health] = 10;
+dungeon[zombie]  [damage] = dmg(str());
+dungeon[zombie]  [dexterity] = dex();
+dungeon[zombie]  [strength] = str();
+
 dungeon[beholder] = [type];
 dungeon[beholder] = [health];
 dungeon[beholder] = [damage];
 dungeon[beholder] = [dexterity];
 dungeon[beholder] = [strength];
 
+dungeon[beholder]  [type] = "Beholder";
+dungeon[beholder]  [health] = 75;
+dungeon[beholder]  [damage] = dmg(str());
+dungeon[beholder]  [dexterity] = dex();
+dungeon[beholder]  [strength] = str();
 
 
 
 // Castle Array
 orc = 0;
-skeleton = type;
-ghost = health;
-dragon = damage;
-knight = dexterity;
-king = strength;
+ghost = 1;
+dragon = 2;
+knight = 3;
+king = 4;
 
 castle = [];
 castle[orc] = [];
-castle[skeleton] = [];
 castle[ghost] = [];
 castle[dragon] = [];
 castle[knight] = [];
 castle[king] = [];
 
 
-cave[orc] = [type];
-cave[orc] = [health];
-cave[orc] = [damage];
-cave[orc] = [dexterity];
-cave[orc] = [strength];
+castle[orc] = [type];
+castle[orc] = [health];
+castle[orc] = [damage];
+castle[orc] = [dexterity];
+castle[orc] = [strength];
 
-cave[skeleton] = [type];
-cave[skeleton] = [health];
-cave[skeleton] = [damage];
-cave[skeleton] = [dexterity];
-cave[skeleton] = [strength];
+castle[orc]  [type] = "Orc";
+castle[orc]  [health] = 5;
+castle[orc]  [damage] = dmg(str());
+castle[orc]  [dexterity] = dex();
+castle[orc]  [strength] = str();
 
-cave[ghost] = [type];
-cave[ghost] = [health];
-cave[ghost] = [damage];
-cave[ghost] = [dexterity];
-cave[ghost] = [strength];
+castle[ghost] = [type];
+castle[ghost] = [health];
+castle[ghost] = [damage];
+castle[ghost] = [dexterity];
+castle[ghost] = [strength];
 
-cave[dragon] = [type];
-cave[dragon] = [health];
-cave[dragon] = [damage];
-cave[dragon] = [dexterity];
-cave[dragon] = [strength];
+castle[ghost]  [type] = "Ghost";
+castle[ghost]  [health] = 8;
+castle[ghost]  [damage] = dmg(str());
+castle[ghost]  [dexterity] = dex();
+castle[ghost]  [strength] = str();
 
-cave[knight] = [type];
-cave[knight] = [health];
-cave[knight] = [damage];
-cave[knight] = [dexterity];
-cave[knight] = [strength];
+castle[dragon] = [type];
+castle[dragon] = [health];
+castle[dragon] = [damage];
+castle[dragon] = [dexterity];
+castle[dragon] = [strength];
 
-cave[king] = [type];
-cave[king] = [health];
-cave[king] = [damage];
-cave[king] = [dexterity];
-cave[king] = [strength];
+castle[dragon]  [type] = "Dragon";
+castle[dragon]  [health] = 500;
+castle[dragon]  [damage] = dmg(str());
+castle[dragon]  [dexterity] = dex();
+castle[dragon]  [strength] = str();
 
+
+
+castle[knight] = [type];
+castle[knight] = [health];
+castle[knight] = [damage];
+castle[knight] = [dexterity];
+castle[knight] = [strength];
+
+castle[knight]  [type] = "Knight";
+castle[knight]  [health] = 15;
+castle[knight]  [damage] = dmg(str());
+castle[knight]  [dexterity] = dex();
+castle[knight]  [strength] = str();
+
+
+
+castle[king] = [type];
+castle[king] = [health];
+castle[king] = [damage];
+castle[king] = [dexterity];
+castle[king] = [strength];
+
+castle[king]  [type] = "King";
+castle[king]  [health] = 30;
+castle[king]  [damage] = dmg(str());
+castle[king]  [dexterity] = dex();
+castle[king]  [strength] = str();
+
+
+console.log(castle[king][damage]);
--- WPF/Functions_Assignment/Wacky/Javascript/WackyFunction.js
+++ WPF/Functions_Assignment/Wacky/Javascript/WackyFunction.js
@@ -5,6 +5,7 @@
 
 
 var PlayersTeam= "";
+var mission = prompt("Cave, Dungeon or Castle").toLowerCase();
 
 function Team(playersTeam){
 var whileCounter = 1;
@@ -33,14 +34,221 @@ return playersTeam ;
 }
 
 console.log("Your team consists of a "+ Team(PlayersTeam));
-
-var mission = prompt("Cave, Dungeon or Castle");
-
+console.log(mission+":");
 
 
+function caveMonsters(cave){
+    bear1 = .05; // 0 - .05 = .05
+    yeti1 = .13; // .05 - .13   = .08
+    mudman1 = .3; // .13 - .3 = .27
+    skeleton1 = .65; // .3 - .65
+    bat1 = 1; //  // .65 - 1
+
+    var monstersTeam = "";
+    var monsterStats= "";
+
+    var counter = 0;
+    for (MonsterGen=0;MonsterGen<3;MonsterGen++){
+
+if (counter == 1){monstersTeam+= "a "} else if(counter == 2){monstersTeam+="and a "}
+
+    var randomNumber = Math.random();
+        if (randomNumber<bear1){
+            monstersTeam+= cave[bear][type]+", ";
+            monsterStats+= "Bear: "+"\n"+
+                           "Str: "+cave[bear][strength] +"\n"+
+                           "Dex: "+cave[bear][dexterity] +"\n"+
+                           "Health: "+cave[bear][health]+"\n"+
+                           "Damage: "+cave[bear][damage]+"\n\n";
+        } else if (randomNumber>bear1 && randomNumber<yeti1){
+            monstersTeam+= cave[yeti][type]+", ";
+            monsterStats+= "Yeti: "+"\n"+
+                           "Str: "+cave[yeti][strength] +"\n"+
+                           "Dex: "+cave[yeti][dexterity] +"\n"+
+                           "Health: "+cave[yeti][health]+"\n"+
+                           "Damage: "+cave[yeti][damage]+"\n\n";
+        } else if (randomNumber>yeti1 && randomNumber<mudman1){
+            monstersTeam+= cave[mudman][type]+", ";
+            monsterStats+= "Mudman:"+"\n"+
+                           "Str: "+cave[mudman][strength] +"\n"+
+                           "Dex: "+cave[mudman][dexterity] +"\n"+
+                           "Health: "+cave[mudman][health]+"\n"+
+                           "Damage: "+cave[mudman][damage]+"\n\n";
+        } else if (randomNumber>mudman1 && randomNumber<skeleton1){
+            monstersTeam+= cave[skeleton][type]+", ";
+            monsterStats+= "Skeleton:"+"\n"+
+                           "Str: "+cave[skeleton][strength] +"\n"+
+                           "Dex: "+cave[skeleton][dexterity] +"\n"+
+                           "Health: "+cave[skeleton][health]+"\n"+
+                           "Damage: "+cave[skeleton][damage]+"\n\n";
+        } else {
+            monstersTeam+= cave[bat][type]+", ";
+            monsterStats+= "Bat: "+"\n"+
+                           "Str: "+cave[bat][strength] +"\n"+
+                           "Dex: "+cave[bat][dexterity] +"\n"+
+                           "Health: "+cave[bat][health]+"\n"+
+                           "Damage: "+cave[bat][damage]+"\n\n";
+        }
 
 
-function fightGeneration(mission ,dungeon, castle, cave){
+
+        counter++
+    }
+    monstersTeam+= "\n";
+    return monstersTeam+ "\n" +
+           monsterStats;
+
+}
 
 
+
+
+
+
+
+
+function dungeonMonster(dungeon){
+    beholder1 = .05; // 0 - .05 = .05
+    blob1 = .13; // .05 - .13   = .08
+    zombie1 = .3; // .13 - .3 = .27
+    skeleton1 = .65; // .3 - .65
+    orc1 = 1; //  // .65 - 1
+
+    var monstersTeam = "";
+    var monsterStats= "";
+
+    var counter = 0;
+    for (MonsterGen=0;MonsterGen<3;MonsterGen++){
+
+        if (counter == 1){monstersTeam+= "a "} else if(counter == 2){monstersTeam+="and a "}
+
+        var randomNumber = Math.random();
+        if (randomNumber<beholder1){
+            monstersTeam+= dungeon[beholder][type]+", ";
+            monsterStats+= "Beholder: "+"\n"+
+                "Str: "+dungeon[beholder][strength] +"\n"+
+                "Dex: "+dungeon[beholder][dexterity] +"\n"+
+                "Health: "+dungeon[beholder][health]+"\n"+
+                "Damage: "+dungeon[beholder][damage]+"\n\n";
+        } else if (randomNumber>beholder1 && randomNumber<blob1){
+            monstersTeam+= dungeon[blob][type]+", ";
+            monsterStats+= "Blob: "+"\n"+
+                "Str: "+dungeon[blob][strength] +"\n"+
+                "Dex: "+dungeon[blob][dexterity] +"\n"+
+                "Health: "+dungeon[blob][health]+"\n"+
+                "Damage: "+dungeon[blob][damage]+"\n\n";
+        } else if (randomNumber>blob1 && randomNumber<zombie1){
+            monstersTeam+= dungeon[zombie][type]+", ";
+            monsterStats+= "Zombie:"+"\n"+
+                "Str: "+dungeon[zombie][strength] +"\n"+
+                "Dex: "+dungeon[zombie][dexterity] +"\n"+
+                "Health: "+dungeon[zombie][health]+"\n"+
+                "Damage: "+dungeon[zombie][damage]+"\n\n";
+        } else if (randomNumber>zombie1 && randomNumber<skeleton1){
+            monstersTeam+= dungeon[skeleton][type]+", ";
+            monsterStats+= "Skeleton:"+"\n"+
+                "Str: "+dungeon[skeleton][strength] +"\n"+
+                "Dex: "+dungeon[skeleton][dexterity] +"\n"+
+                "Health: "+dungeon[skeleton][health]+"\n"+
+                "Damage: "+dungeon[skeleton][damage]+"\n\n";
+        } else {
+            monstersTeam+= dungeon[orc][type]+", ";
+            monsterStats+= "Orc: "+"\n"+
+                "Str: "+dungeon[orc][strength] +"\n"+
+                "Dex: "+dungeon[orc][dexterity] +"\n"+
+                "Health: "+dungeon[orc][health]+"\n"+
+                "Damage: "+dungeon[orc][damage]+"\n\n";
+        }
+
+
+
+        counter++
+    }
+    monstersTeam+= "\n";
+    return monstersTeam+ "\n" +
+        monsterStats;
+
+}
+
+
+
+
+
+function castleMonsters(castle){
+    dragon1 = .05; // 0 - .05 = .05
+    king1 = .13; // .05 - .13   = .08
+    knight1 = .3; // .13 - .3 = .27
+    ghost1 = .65; // .3 - .65
+    orc1 = 1; //  // .65 - 1
+
+    var monstersTeam = "";
+    var monsterStats= "";
+
+    var counter = 0;
+    for (MonsterGen=0;MonsterGen<3;MonsterGen++){
+
+        if (counter == 1){monstersTeam+= "a "} else if(counter == 2){monstersTeam+="and a "}
+
+        var randomNumber = Math.random();
+        if (randomNumber<dragon1){
+            monstersTeam+= castle[dragon][type]+", ";
+            monsterStats+= "Dragon: "+"\n"+
+                "Str: "+castle[dragon][strength] +"\n"+
+                "Dex: "+castle[dragon][dexterity] +"\n"+
+                "Health: "+castle[dragon][health]+"\n"+
+                "Damage: "+castle[dragon][damage]+"\n\n";
+        } else if (randomNumber>dragon1 && randomNumber<king1){
+            monstersTeam+= castle[king][type]+", ";
+            monsterStats+= "King: "+"\n"+
+                "Str: "+castle[king][strength] +"\n"+
+                "Dex: "+castle[king][dexterity] +"\n"+
+                "Health: "+castle[king][health]+"\n"+
+                "Damage: "+castle[king][damage]+"\n\n";
+        } else if (randomNumber>king1 && randomNumber<knight1){
+            monstersTeam+= castle[knight][type]+", ";
+            monsterStats+= "Knight:"+"\n"+
+                "Str: "+castle[knight][strength] +"\n"+
+                "Dex: "+castle[knight][dexterity] +"\n"+
+                "Health: "+castle[knight][health]+"\n"+
+                "Damage: "+castle[knight][damage]+"\n\n";
+        } else if (randomNumber>knight1 && randomNumber<ghost1){
+            monstersTeam+= castle[ghost][type]+", ";
+            monsterStats+= "Ghost:"+"\n"+
+                "Str: "+castle[ghost][strength] +"\n"+
+                "Dex: "+castle[ghost][dexterity] +"\n"+
+                "Health: "+castle[ghost][health]+"\n"+
+                "Damage: "+castle[ghost][damage]+"\n\n";
+        } else {
+            monstersTeam+= castle[orc][type]+", ";
+            monsterStats+= "Orc: "+"\n"+
+                "Str: "+castle[orc][strength] +"\n"+
+                "Dex: "+castle[orc][dexterity] +"\n"+
+                "Health: "+castle[orc][health]+"\n"+
+                "Damage: "+castle[orc][damage]+"\n\n";
+        }
+
+
+
+        counter++
+    }
+    monstersTeam+= "\n";
+    return monstersTeam+ "\n" +
+        monsterStats;
+
+}
+
+
+
+
+
+
+
+if (mission == "cave"){
+    console.log("You have encountered a "+ caveMonsters(cave));
+} else if (mission == "dungeon"){
+    console.log("You have encountered a "+ dungeonMonster(dungeon));
+} else if (mission == "castle"){
+    console.log("You have encountered a "+ castleMonsters(castle));
+} else {
+    console.log("");
 }
