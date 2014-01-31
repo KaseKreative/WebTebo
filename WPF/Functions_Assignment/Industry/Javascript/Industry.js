c2b97843a121ebf0fec30771cadd1f2ddccd5df3
KaseKreative <KaseKreative@fullsail.edu>
2014-01-30 20:27:35 -0500

    FunctionsAssignment11

--- WPF/Functions_Assignment/Industry/Javascript/Industry.js
+++ WPF/Functions_Assignment/Industry/Javascript/Industry.js
@@ -2,12 +2,16 @@
  * Created by KaseTebo on 1/30/14.
  */
 console.log("What are you trying to program?");
-var what =prompt ("Game, Software, Plugin");
-var simpleOrComplex = prompt("Simple or Complex?");
+var what =prompt ("Game, Software, Plugin").toLowerCase();
+var simpleOrComplex = prompt("Simple or Complex?").toLowerCase();
 
 
 
 function time (what, simpleOrComplex){
 
-    
+    if (what == "game" && simpleOrComplex == "simple"){
+
+    }
+
+
 }
