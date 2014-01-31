6e6000ac1ef33e855faa6fa073ea56b45374f3a7
KaseKreative <KaseKreative@fullsail.edu>
2014-01-30 20:32:09 -0500

    FunctionsAssignment13

--- WPF/Functions_Assignment/Industry/Javascript/Industry.js
+++ WPF/Functions_Assignment/Industry/Javascript/Industry.js
@@ -11,9 +11,11 @@ function time (what, simpleOrComplex){
 
     if (what == "game" && simpleOrComplex == "simple"){
             return "A simple game can take up to a week";
-    }   else if (what == "game" && simpleOrComplex == "complex"){
+    } else if (what == "game" && simpleOrComplex == "complex"){
             return "I am not sure how long a game takes to program probably a while"
-    }
+    } else if (what == "software" && simpleOrComplex == "simple"){
+            return "A simple software can take anywhere from an hour to a week";
+    } else if (what == "software"){}
 
 
 }
