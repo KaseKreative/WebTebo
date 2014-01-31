14eff5a2de66cf003d59d11b1e7c3ce84a4ec89e
KaseKreative <KaseKreative@fullsail.edu>
2014-01-30 20:30:31 -0500

    FunctionsAssignment12

--- WPF/Functions_Assignment/Industry/Javascript/Industry.js
+++ WPF/Functions_Assignment/Industry/Javascript/Industry.js
@@ -10,7 +10,9 @@ var simpleOrComplex = prompt("Simple or Complex?").toLowerCase();
 function time (what, simpleOrComplex){
 
     if (what == "game" && simpleOrComplex == "simple"){
-
+            return "A simple game can take up to a week";
+    }   else if (what == "game" && simpleOrComplex == "complex"){
+            return "I am not sure how long a game takes to program probably a while"
     }
 
 
