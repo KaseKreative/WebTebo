f30bdde2f6308912f21fde15ba611aa756521355
KaseKreative <KaseKreative@fullsail.edu>
2014-03-27 12:00:44 -0400

    Final

--- DWS1/RandomRestaurant/js/Main.js
+++ DWS1/RandomRestaurant/js/Main.js
@@ -4,39 +4,18 @@
 
 (function(){
 
-   var input = document.querySelector("#input");
-   var button = document.querySelector("#button");
+   var input = document.getElementsByName("drinking");
+   var button = document.querySelector("#submit");
    var header = document.querySelector("#header");
 
-    button.addEventListener("click", onClick);
+//    button.addEventListener("click", onClick);
 
-    console.log(input.value);
 
-function onClick(){
-    var drinking = Drinking(input.toLowerCase());
 
-    function Drinking (input){
-        var drinking = 0;
-        if (input == "yes"){
-            drinking = 1;
-        } else if (input = "no"){
-             drinking = 2;
-        } else {
-            drinking = 3;
-        }
-        return drinking;
-    }
+    function onClick (){
+        console.log(input.checked);
 
-    function Car (drinking){
-        if (drinking == 1 || drinking == 2){
-          header.innerHTML = "Do you have a car?";
-        } else {
-          header.innerHTML = "Incorrect Answer";
-        }
 
     }
-
-    Car(drinking);
-}
 
 })();
